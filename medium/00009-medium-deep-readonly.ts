df2e3b09c1052b014952fac53e2375e0e0a5de96
daizhitao <daizhitao@dlz-tech.com>
2023-12-08 15:59:36 +0800

    feat: type-challenges 00010、00012

--- medium/00009-medium-deep-readonly.ts
+++ medium/00009-medium-deep-readonly.ts
@@ -8,8 +8,7 @@
   Implement a generic `DeepReadonly<T>` which make every parameter of an object - and its sub-objects recursively - readonly.
 
   You can assume that we are only dealing with Objects in this challenge. Arrays, Functions, Classes and so on do not need to be taken into consideration. However, you can still challenge yourself by covering as many different cases as possible.
-
-  For example:
+00
 
   ```ts
   type X = {
